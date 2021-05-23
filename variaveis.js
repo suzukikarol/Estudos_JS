da32d7cbb301a44a95c52edda5200f161a84f077
Karoline Suzuki <ka.suzuki.23@gmail.com>
2021-05-23 15:23:20 -0300

    Adicionando arquivo sobre variáveis em js

---
+++ variaveis.js
@@ -0,0 +1,24 @@
+// Não é necessário utilizar a palavra reservada var para declarar uma variável, mas é uma boa prática.
+
+//exemplo de como declarar uma variável numérica de valor inteiro
+//neste caso a variável num esta recebendo o valor 1
+var num = 1;
+
+//seguindo o exemplo acima, aqui a mesma variável está recebendo outro valor
+num = 3;
+
+//quando for declarar variável do tipo float, ou valores numéricos que não são inteiros, usase o ponto(.) no lugar da vírgula
+var price = 1.5;
+
+//para declarar variável do tipo texto/string coloca o valor entre aspas
+var myName = 'Karol';
+
+//declarando valores booleanos, de true ou false
+var trueValue = true;
+
+//declarando valor de null/nulo
+var nullValue = null;
+
+//declando uma variável do tipo undefined/indefinido
+var und;
+
